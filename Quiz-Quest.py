372e96bfb89a31ae88f86326e5450404ceaea88b
Mitchell Javins <MJavins@stu.kerikerihigh.ac.nz>
2021-07-01 10:07:54 +1200

    3rd commit

--- Quiz-Quest.py
+++ Quiz-Quest.py
@@ -1,7 +1,7 @@
 #yuuuuuuuuuuuuu
 import keyboard
 print("Welcome to the league of legends quiz. \nPress Y to play, or press N to leave")
-while True:
+while not True:
     try:
         if keyboard.is_pressed('y'):
             print("I Hope you enjoy the quiz ;)")
@@ -18,8 +18,25 @@ quest1 = input("Question 1, Who is the Iron Revenant\n\
     2 - Tahm Kench \
     3 - Garen\
     4 - Kayn\n")
+while not True:
+    try:
+        if quest1 == 1:
+            print("Congrats, you got it right")
+            break
+        if quest1 != 1:
+            print("That was incorrect, please restart the quiz")
+            exit()
 
-if quest1 == 1:
-    print("Congrats, you got it right")
-    else:
-    print("Incorrect, you fail, please restart the quiz")
+quest2 = input("Question 2, Who's ultimate sucks every enemy in, then fires a beam of moon light on them\n\
+    1 - Yorick\
+    2 - Kayle\
+    3 - Diana\
+    4 - Kled\n")
+while not True:
+    try:
+        if quest2 == 3:
+            print("Congrats, that was correct")
+            break
+        if quest2 != 3:
+            print("That was incorrect, please restart the quiz")
+            exit()
